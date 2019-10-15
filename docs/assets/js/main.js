d8357fb163f977151bc5a82f23501eddb0626e88
zacharyweaver <zaclweaver@gmail.com>
2019-10-15 04:22:46 -0400

    Update 10-14-19

---
+++ docs/assets/js/main.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+  v2compatible: true,
+  autoScrolling: true,
+  navigation: true,
+  navigationPosition: 'bottom',
+  loopBottom: true,
+  menu: '#menu',
+  anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
+  controlArrows: true
+})
