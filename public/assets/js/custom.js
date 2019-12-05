f09319ea63288dc19399ce1270993cc9c3393f82
zacharyweaver <zaclweaver@gmail.com>
2019-12-05 00:27:37 -0500

    team bug needs fixed

---
+++ public/assets/js/custom.js
@@ -0,0 +1,7 @@
+//parallax library init
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true,
+  clipRelativeInput: true,
+  hoverOnly:true,
+});
